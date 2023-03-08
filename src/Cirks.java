36625fe53579fe045963dbd49a733a228dee5c8c
Patriks <patriks.venens@gmail.com>
2023-03-08 09:55:15 +0200

    veiktas izmainas

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,13 +1,21 @@
 import java.util.Random;
 
 public class Cirks {
-
+	public static  int mestKaulinu(){
+		
+		int skaitlis;
+		Random rand = new Random();
+		 skaitlis= rand.nextInt(6)+1;
+		 System.out.println("tavs cipars: "+skaitlis);
+		 
 	public static void main(String[] args) {
-	int skaitlis;
-	 Random rand = new Random();
-	 skaitlis= rand.nextInt(6)+1;
-	 System.out.println("tavs cipars: "+skaitlis);
+	
+		mestKaulinu();
 
 	}
+	
+		
+	}
+	
 
 }
