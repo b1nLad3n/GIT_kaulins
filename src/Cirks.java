f2e2c824df84496f6565e63515b2d84bdddc137e
Patriks <patriks.venens@gmail.com>
2023-03-20 15:24:57 +0200

    Izlabotas kludas

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,21 +1,28 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Cirks {
-	public static  int mestKaulinu(){
+	public static  void mestKaulinu(int reizes){
 		
-		int skaitlis;
+		int skaitlis=0;
 		Random rand = new Random();
+		for(int i=1; i<=reizes;i++){
 		 skaitlis= rand.nextInt(6)+1;
 		 System.out.println("tavs cipars: "+skaitlis);
+		}
 		 
+		
+	}
 	public static void main(String[] args) {
-	
-		mestKaulinu();
-
+	int reizes;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reize matis kaulin? ");
+		reizes = scan.nextInt();
+		mestKaulinu(reizes);
 	}
 	
 		
 	}
 	
 
-}
+
